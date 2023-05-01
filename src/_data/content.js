0437d23593e88756d56a591200a96480c7dacf57
AlmantasK <almantusk@gmail.com>
2023-05-01 15:26:01 +0300

    chore: antique philosophers and some more

--- src/_data/content.js
+++ src/_data/content.js
@@ -1,48 +1,190 @@
-const header = 'Welcome to this wonderful example timeline.';
-const footer = 'A footer goes here.';
+
+const header = 'Philosophers of the ages';
+const footer = '';
 const entries = [
   {
-    id: 'cat',
-    categories: ['cat'],
+    id: 'Pythagoras',
+    categories: ['Abstracionism'],
     color: 'green',
-    faicon: 'cat',
-    datetime: '2021-01-01 05:00',
-    title: 'Cats are very good',
+    faicon: 'dove',
+    datetime: '570-01-01',
+    title: 'Pythagoras',
     image: {
-      link: 'http://placekitten.com',
-      src: 'http://placekitten.com/200/300',
-      alt: 'A placeholder kitten',
-      caption: 'Kitten!',
+      link: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Pythagoras_in_the_Roman_Forum%2C_Colosseum.jpg/220px-Pythagoras_in_the_Roman_Forum%2C_Colosseum.jpg',
+      src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Pythagoras_in_the_Roman_Forum%2C_Colosseum.jpg/220px-Pythagoras_in_the_Roman_Forum%2C_Colosseum.jpg',
+      alt: 'Pythagoras',
+      caption: 'Pythagoras',
     },
-    body: "The best thing in the universe is a cardboard box. Chirp at birds catasstrophe for meowwww for hiding behind the couch until lured out by a feathery toy for morning beauty routine of licking self sugar, my siamese, stalks me (in a good way), day and night lick the other cats. Pushed the mug off the table shove bum in owner's face like camera lens or tickle my belly at your own peril i will pester for food when you're in the kitchen even if it's salad and grass smells good, licks your face, but hiiiiiiiiii feed me now.",
+    body: "Abstract ideas are perfect. Its' simplicity and elegance are the primary criterias of it being right. It doesn't have to represent the reality as we see it today, for we see too little. An equation representing a concept today may mean nothing practical, but be applied decades later. Had a cult who worshiped numbers.",
     links: [
       {
-        href: 'https://en.wikipedia.org/wiki/Cat',
-        linkText: 'Cat',
+        href: 'https://en.wikipedia.org/wiki/Pythagoras',
+        linkText: 'Wiki',
+      },
+    ],
+  },
+    {
+    id: 'Socrates',
+    categories: ['Stoicism'],
+    color: 'green',
+    faicon: 'dove',
+    datetime: '-470-01-01',
+    title: 'Socrates',
+    image: {
+      link: 'https://ecdn.teacherspayteachers.com/thumbitem/Lesson-The-Life-and-Death-of-the-Greek-philosopher-Socrates-1657510534/original-274489-1.jpg',
+      src: 'https://ecdn.teacherspayteachers.com/thumbitem/Lesson-The-Life-and-Death-of-the-Greek-philosopher-Socrates-1657510534/original-274489-1.jpg',
+      alt: 'Pythagoras',
+      caption: 'Socrates',
+    },
+    body: "Loved asking deep questions (to other people), until they would start contradicting themselves (socratic method). He claimed he was ignorant and knew nothing. Oracle of Deplhi called him the smartest of all - for he has admitted knowing nothing yet striving to learn more. Did not document his teachings. When caputred, he did not give into his principles and for that reason was executed.",
+    links: [
+      {
+        href: 'https://en.wikipedia.org/wiki/Socrates',
+        linkText: 'Wiki',
+      },
+    ],
+  },
+    {
+    id: 'Plato',
+    categories: ['Abstracionism','Stoicism'],
+    color: 'green',
+    faicon: 'dove',
+    datetime: '-428-01-01',
+    title: 'Plato',
+    image: {
+      link: 'https://cdn.britannica.com/88/149188-050-DC34842F/Plato-portrait-bust-original-Capitoline-Museums-Rome.jpg',
+      src: 'https://cdn.britannica.com/88/149188-050-DC34842F/Plato-portrait-bust-original-Capitoline-Museums-Rome.jpg',
+      alt: 'Plato',
+      caption: 'Plato',
+    },
+    body: "Forms: Justice, Beauty, Equality. Metaphysics and abstract ideas. A part of Socrates cirlce. Documented Socrates work.",
+    links: [
+      {
+        href: 'https://en.wikipedia.org/wiki/Plato',
+        linkText: 'Wiki',
+      },
+    ],
+  },
+    {
+    id: 'Aristotle',
+    categories: ['Empiricism', 'Stoicism'],
+    color: 'green',
+    faicon: 'dove',
+    datetime: '-384-01-01',
+    title: 'Aristotle',
+    image: {
+      link: 'https://hips.hearstapps.com/hmg-prod/images/aristotle--getty.jpg?crop=1xw:0.75xh;center,top&resize=1200:*',
+      src: 'https://hips.hearstapps.com/hmg-prod/images/aristotle--getty.jpg?crop=1xw:0.75xh;center,top&resize=1200:*',
+      alt: 'Aristotle',
+      caption: 'Aristotle',
+    },
+    body: "Father of a lot of nature sciences. Formulated a definition of logic. To truly understand things one should touch them, feel them. Experience is important. The true joy of life is self-development.",
+  },
+    {
+    id: 'Pyrrho',
+    categories: ['Scepticism'],
+    color: 'green',
+    faicon: 'dove',
+    datetime: '-365-01-01',
+    title: 'Pyrrho',
+    image: {
+      link: 'https://historyofphilosophy.net/sites/default/files/styles/medium/public/images/podcast_image/western/070%20Carneades.jpg?itok=sx3fwIHb',
+      src: 'https://historyofphilosophy.net/sites/default/files/styles/medium/public/images/podcast_image/western/070%20Carneades.jpg?itok=sx3fwIHb',
+      alt: 'Pyrrho',
+      caption: 'Pyrrho',
+    },
+    body: "Nothing can be trusted (people, senses, yourself). Would have died by going of cliff many times (volunterally, by not trusting senses) if not for his friends. Father of skepticism.",
+    links: [
+      {
+        href: 'https://en.wikipedia.org/wiki/Pyrrho',
+        linkText: 'Wiki',
       },
     ],
   },
   {
-    id: 'dog',
-    categories: ['dog'],
+    id: 'Epicurus',
+    categories: ['Stoicism'],
     color: 'green',
-    faicon: 'dog',
-    datetime: '2021-05-01 23:55',
-    title: 'Puppies are great too',
+    faicon: 'dove',
+    datetime: '-270-01-01',
+    title: 'Epicurus',
     image: {
-      link: 'http://place-puppy.com',
-      src: 'https://place-puppy.com/300x300',
-      alt: 'A placeholder puppy',
-      caption: 'Puppy!',
+      link: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Epikouros_BM_1843.jpg/800px-Epikouros_BM_1843.jpg',
+      src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Epikouros_BM_1843.jpg/800px-Epikouros_BM_1843.jpg',
+      alt: 'Epicurus',
+      caption: 'Epicurus',
     },
-    body: 'Doggo ipsum sub woofer smol wow very biscit aqua doggo pupper dat tungg tho big ol pupper, very jealous pupper mlem heckin angery woofer very jealous pupper. Blep waggy wags long doggo, corgo. Tungg wow such tempt mlem very jealous pupper boofers lotsa pats, snoot smol big ol ruff doge super chub, long doggo heckin good boys and girls h*ck heck. Shoober blop many pats borkf, such treat. Big ol what a nice floof long water shoob wrinkler heck sub woofer, pupper porgo wow very biscit.',
+    body: "The garden of life. Human growing like a flower, blooming when self-developing. The simplicity of life, self sufficiency.",
     links: [
       {
-        href: 'https://en.wikipedia.org/wiki/Dog',
-        linkText: 'Dog',
+        href: 'https://en.wikipedia.org/wiki/Epicurus',
+        linkText: 'Wiki',
       },
     ],
   },
+  {
+    id: 'Seneca',
+    categories: ['Stoicism', 'Politics'],
+    color: 'green',
+    faicon: 'landmark',
+    datetime: '65-01-01',
+    title: 'Seneca',
+    image: {
+      link: 'https://cdn.britannica.com/33/9433-050-C21D7F4F/marble-bust-Seneca-Staatliche-Museen-Berlin-Germany.jpg?w=400&h=300&c=crop',
+      src: 'https://cdn.britannica.com/33/9433-050-C21D7F4F/marble-bust-Seneca-Staatliche-Museen-Berlin-Germany.jpg?w=400&h=300&c=crop',
+      alt: 'Seneca',
+      caption: 'Seneca',
+    },
+    body: "One of the greatest stoics. Seneca's letters and other great works of literature (TODO).",
+    links: [
+      {
+        href: 'https://en.wikipedia.org/wiki/Plato',
+        linkText: 'Wiki',
+      },
+    ],
+  },
+  {
+    id: 'JohnStuartMill',
+    categories: ['Utilitariasm', 'Politics', 'Feminism'],
+    color: 'green',
+    faicon: 'feather-pointed',
+    datetime: '1806-01-01',
+    title: 'John Stuart Mill',
+    image: {
+      link: 'https://upload.wikimedia.org/wikipedia/commons/7/75/JohnStuartMill.jpg',
+      src: 'https://upload.wikimedia.org/wikipedia/commons/7/75/JohnStuartMill.jpg',
+      alt: 'John Stuart Mill',
+      caption: 'John Stuart Mill',
+    },
+    body: "A genius boy (raised as a genius as an experiment by his father), inspired by Bentham developing ideas of utilitariasm. Idea of higher and lower pleasures: it's better to be a sad human than a happy pig. One of the first feminists, fighters of equality. People should have all their freedoms unless those freedoms hurt others.",
+    links: [
+      {
+        href: 'https://en.wikipedia.org/wiki/John_Stuart_Mill',
+        linkText: 'Wiki',
+      },
+    ],
+  },
+  {
+    id: 'JeremyBentham',
+    categories: ['Utilitariasm', 'Animal', 'Imperialism'],
+    color: 'green',
+    faicon: 'feather-pointed',
+    datetime: '1748-01-01',
+    title: 'Jeremy Bentham',
+    image: {
+      link: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c8/Jeremy_Bentham_by_Henry_William_Pickersgill_detail.jpg/220px-Jeremy_Bentham_by_Henry_William_Pickersgill_detail.jpg',
+      src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c8/Jeremy_Bentham_by_Henry_William_Pickersgill_detail.jpg/220px-Jeremy_Bentham_by_Henry_William_Pickersgill_detail.jpg',
+      alt: 'Jeremy Bentham',
+      caption: 'Jeremy Bentham',
+    },
+    body: "Father of utilitariasm. Meaning of life is to be happy. Inspired by Epicurus. Fighter for animal rights (first such philosopher)",
+    links: [
+      {
+        href: 'https://en.wikipedia.org/wiki/Jeremy_Bentham',
+        linkText: 'Wiki',
+      },
+    ],
+  }, 
 ];
 
 // Page details
